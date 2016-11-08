7b12141c2ea0a29165b2c93906b6620b42c82a8b
Anthony Fermin <afermin@fuzzproductions.com>
2016-11-08 13:08:56 -0500

    added annotation processing to generate GraphQL query

--- app/src/main/java/com/fuzz/anthonyfermin/starwarsgraphql/MainActivity.java
+++ app/src/main/java/com/fuzz/anthonyfermin/starwarsgraphql/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.anthonyfdev.graphQLQueryGen.models.GraphQL_GraphQLData;
 import com.fuzz.anthonyfermin.starwarsgraphql.model.GraphQLResponse;
 
 import java.io.IOException;
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(Void... params) {
-            String graphQLRequest = "{ allFilms { totalCount } allPeople { totalCount } }";
+            String graphQLRequest = GraphQL_GraphQLData.getQuery();
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl("http://graphql-swapi.parseapp.com")
                     .client(new OkHttpClient.Builder().addInterceptor(new Interceptor() {
